024be23439190a89e20868936f2968dd7c10ab64
kollepara karthik <110515286+2100030263@users.noreply.github.com>
2024-01-03 11:27:46 +0530

    Update Karthik.java

--- Karthik.java
+++ Karthik.java
@@ -3,6 +3,6 @@ public class Karthik
     public static void main(String []args)
     {
       System.out.println("HELLO WORLD");
-        System.out.println("HELLO WORLD");
+        System.out.println("I am karthik");
     }
   }
